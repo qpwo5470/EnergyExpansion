4a8680fe68bee4250ef3512d9f4c4d673fac13ad
qpwo5470 <qpwo5470@gmail.com>
2021-07-22 01:03:55 +0900

    first

--- main.py
+++ main.py
@@ -11,8 +11,14 @@ keys = ['H', 'S', 'P']
 baud = 9600
 music = AudioPlayer('music.mp3')
 
+serials = []
+threads = []
 
-def serialthread(index):
+for port in ports:
+    serials.append(serial.Serial(port, baud, timeout=0))
+
+
+def serialthread(ser):
     global ports
     global keys
     global music
@@ -20,42 +26,26 @@ def serialthread(index):
     i = '-'
 
     while True:
-        try:
-            ser = serial.Serial(ports[index], baud, timeout=0)
-            while True:
-                try:
-                    for c in ser.read_until():
-                        c = chr(c)
-                        print(f'{c} in {index}')
-                        if c in keys:
-                            ser.write(b'K')
-                            i = c
-                            print(f'{index} is {i}')
-                        if c == 'B':
-                            if i == 'S':
-                                music.play(block=False)
-                                time.sleep(10)
-                                music.stop()
-                            else:
-                                ser.write(b'N')
-                                time.sleep(10)
-                                ser.write(b'F')
-                                ser.reset_input_buffer()
-                except:
-                    pass
-
-
-        except:
-            print(f'cant connect {index}')
-            pass
+        for c in ser.read_until():
+            c = chr(c)
+            print(f'{c} in {i}')
+            if c in keys:
+                ser.write(b'K')
+                i = c
+            if c == 'B':
+                if i == 'S':
+                    music.play(block=False)
+                    time.sleep(10)
+                    music.stop()
+                else:
+                    ser.write(b'N')
+                    time.sleep(10)
+                    ser.write(b'F')
+                    ser.reset_input_buffer()
 
 
 if __name__ == "__main__":
-    serial_threadA = threading.Thread(target=serialthread, args=(0,))
-    serial_threadA.start()
-
-    serial_threadB = threading.Thread(target=serialthread, args=(1,))
-    serial_threadB.start()
-
-    serial_threadC = threading.Thread(target=serialthread, args=(2,))
-    serial_threadC.start()
+    for ser in serials:
+        serial_thread = threading.Thread(target=serialthread, args=(ser,))
+        threads.append(serial_thread)
+        serial_thread.start()
